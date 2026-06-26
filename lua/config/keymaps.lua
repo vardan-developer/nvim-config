b816f5017de6666538e109b7341ea7cfa53a1edd
Vardan Verma <vardan.verma@gravitiontrading.com>
2026-06-26 11:25:06 +0530

    initial nvim config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,48 @@
+local map = vim.keymap.set
+
+vim.g.mapleader = " "        -- space as leader
+vim.g.maplocalleader = " "   -- localleader (used by some plugins)
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+}
+
+-- This is just for the NFS folders saving because otherwise it causes issues in saving xattrs
+vim.cmd([[cnoreabbrev <expr> w  (getcmdtype()==':' && getcmdline()==#'w')  ? 'silent! write'              : 'w']])
+vim.cmd([[cnoreabbrev <expr> wq (getcmdtype()==':' && getcmdline()==#'wq') ? 'silent! write <bar> quit'   : 'wq']])
+vim.cmd([[cnoreabbrev <expr> x  (getcmdtype()==':' && getcmdline()==#'x')  ? 'silent! write <bar> quit'   : 'x']])
+
+map("n", "<M-q>", "<cmd>q<cr>", { desc = "use meta+q to exit panes" })
+
+-- Use ; as the command key in normal and visual mode
+map({ "n", "x" }, ";", ":", { desc = "Command mode" })
+map({ "n", "x" }, "'", ";", { desc = "Repeat find" })
+
+-- Clear search highlight after a search, by pressing Esc
+map("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "Clear search highlight" })
+
+-- Move between split windows with Ctrl + h/j/k/l (no Ctrl-w prefix needed)
+map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+
+-- Move selected lines up/down in visual mode (J/K), keeping selection
+map("v", "J", ":m '>+1<cr>gv=gv", { desc = "Move selection down" })
+map("v", "K", ":m '<-2<cr>gv=gv", { desc = "Move selection up" })
+
+-- Keep cursor centered when jumping half-pages
+map("n", "<C-d>", "<C-d>zz", { desc = "Half page down + center" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Half page up + center" })
+
+-- Move back to last position of cursor using =,-
+map("n", "=", "<C-i>", { desc = "Jump to the previous position you jumped from" })
+map("n", "-", "<C-o>", { desc = "Jump to the next position you jumped to" })
+
+-- Auto recenter after n and N
+map('n', 'n', 'nzz', { desc = "Auto recenter after n" })
+map('n', 'N', 'Nzz', { desc = "Auto recenter after N" })
---
+++ lua/config/options.lua
@@ -0,0 +1,32 @@
+local opt = vim.opt
+
+-- General
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"   -- use the system clipboard
+opt.swapfile = false            -- no swap files (using auto-save.nvim)
+
+-- UI / display
+opt.termguicolors = true        -- enable 24-bit colors (set this early)
+opt.number = true               -- absolute line numbers
+opt.relativenumber = true       -- relative line numbers
+opt.cursorline = true           -- highlight the current line
+opt.wrap = true                 -- wrap long lines
+
+-- Indentation
+opt.autoindent = true
+opt.expandtab = true            -- tabs become spaces
+opt.shiftwidth = 4              -- indent width
+opt.tabstop = 4                 -- a tab counts as 4 spaces
+
+-- Search
+opt.ignorecase = true           -- case-insensitive...
+opt.smartcase = true            -- ...unless the query has uppercase
+
+-- Splits
+opt.splitright = true           -- vertical splits open to the right
+opt.splitbelow = true           -- horizontal splits open below
+
+-- Folding & behavior
+opt.foldlevelstart = 99         -- start with everything unfolded
+opt.formatoptions:append("cro") -- continue comments on new lines (C-u to remove)
+opt.sessionoptions:remove("options")  -- don't persist keymaps/local options
